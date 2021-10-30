666358f20d4daa357368e1d4ed0890d220fd64a9
anona <39059459+WilSegale@users.noreply.github.com>
2021-10-30 15:07:31 -0400

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+import socket,os
+import uuid
+
+os.system('clear') 
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)   
+print("Your Computer Name is:" + hostname)   
+print("Your Computer IP Address is: " + IPAddr) 
+print("your mac address is: " + hex(uuid.getnode()))
